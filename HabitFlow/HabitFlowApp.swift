2ecc0780fe30194563e55fac649fe4edb277bb83
murad <muradkv@gmail.com>
2026-06-20 22:33:07 +0300

    Initial commit

---
+++ HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by murad on 20.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HabitsListView()
+        }
+    }
+}
